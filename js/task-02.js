826dcaa8479b6a2a467b35a446ed75942e02d7e6
Weronika2003 <creative.name2003@gmail.com>
2022-12-13 16:54:34 +0100

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const findUl = document.querySelector("#ingredients");
+
+
+const task = ingredients.forEach((ingredient) => {
+  let ingredientList = document.createElement("li");
+  ingredientList.textContent = ingredient;
+  ingredientList.classList.add("item");
+  findUl.append(ingredientList);
+})
